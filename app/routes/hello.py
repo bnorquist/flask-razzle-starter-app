98f3115822bc16d3d1677a27387e56542325547d
Ben Norquist <norquist.ben@gmail.com>
2019-06-23 12:54:35 -0700

    use marshall_with to return json representation of person class

--- app/routes/hello.py
+++ app/routes/hello.py
@@ -1,19 +1,25 @@
 from flask import Blueprint
 from marshmallow import Schema
-from flask_apispec import marshal_with
+from flask_apispec import marshal_with, use_kwargs
 from webargs import fields
-from webargs.flaskparser import use_args
 
 
 blueprint = Blueprint('hello_page', __name__)
 
 
 class PersonSchema(Schema):
-    name = fields.Str()
+    class Meta:
+        fields = ['name']
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
 
 
 # a simple page that says hello
 @blueprint.route('/api/hello')
-@use_args({'name': fields.Str()})
-def hello(args):
-    return f"Hello {args.get('name')}!"
+@use_kwargs({'name': fields.Str()})
+@marshal_with(PersonSchema)
+def hello(**kwargs):
+    return Person(**kwargs)
