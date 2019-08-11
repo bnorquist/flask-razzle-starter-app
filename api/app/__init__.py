91f3cab3a26c04c081261d0423f24a31df5a31ae
Ben Norquist <norquist.ben@gmail.com>
2019-08-10 18:13:04 -0700

    do not db.create_all() until we have a db connection

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -26,8 +26,9 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-    with app.app_context():
-        db.create_all()
+    # this needs to be called sometime before you interact with the database
+    # with app.app_context():
+    #     db.create_all()
 
     # register blueprints
     app.register_blueprint(hello.blueprint)
