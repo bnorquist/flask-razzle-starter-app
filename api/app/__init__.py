203f3a4c39ce878f3b5feb0b9c23f167c492fd87
Ben Norquist <norquist.ben@gmail.com>
2019-08-10 18:04:03 -0700

    update flask config, do not import app into conftest

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
+from app.models.flask_config import DevelopmentConfig
+from app.models.flask_config import ProductionConfig
 from app.routes import hello
 from flask import Flask
 from flask_apispec.extension import FlaskApiSpec
@@ -10,22 +12,18 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
-def create_app(test_config=None):
+def create_app():
+    if os.environ.get("environment") == "production":
+        config = ProductionConfig()
+    else:
+        config = DevelopmentConfig()
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY="dev",
-        # DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+
+    app.config.from_object(config)
 
     # sqlalchemy db setup
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = config.DATABASE_URI
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     with app.app_context():
